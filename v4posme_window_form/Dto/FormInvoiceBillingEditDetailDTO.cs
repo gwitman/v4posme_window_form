ecfb8826770e9c4c6540f2b6248fb0311f44b0f9
Witman Gonzalez <gwitman>
2024-04-22 20:49:05 -0600

    revision de codigo

---
+++ v4posme_window_form/Dto/FormInvoiceBillingEditDetailDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v4posme_window.Dto
+{
+    public  class FormInvoiceBillingEditDetailDTO
+    {
+        public int TransactionMasterDetailId { get; set; }
+        public int ItemId { get; set; }
+        public string ItemNumber { get; set; }
+        public string TransactionDetailName { get; set; }
+        public int Sku { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Iva { get; set; }
+        public decimal SkuQuantityBySku { get; set; }
+        public decimal UnitaryPriceIndividual { get; set; }
+        public string SkuFormatoDescription { get; set; }
+        public decimal ItemPrecio2 { get; set; }
+        public decimal ItemPrecio3 { get; set; }
+        public string DetailVencimiento { get; set; }
+    }
+}
